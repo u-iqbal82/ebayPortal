aed985c2c03ae84bd6a2b45e582fb0ca7d1a6630
umeriqbal <u_iqbal82@hotmail.com>
2017-11-30 12:31:48 +0000

    nbsp fixed to space

--- app/Http/Controllers/FileImport.php
+++ app/Http/Controllers/FileImport.php
@@ -73,7 +73,7 @@ class FileImport extends Controller
             {
                 $description = str_replace(array("\r\n", "\r", "\n"), '', $article->detail->description);
                 $description = str_replace('<p>&nbsp;</p>', '', $description);
-                $description = str_replace('&nbsp;', '', $description);
+                $description = str_replace('&nbsp;', ' ', $description);
                 $description = str_replace('&amp;', '&', $description);
                 $description = str_replace(array('<br />','<br/>'), '<br>', $description);
                 $description = str_replace("&nbsp;", ' ', $description);
